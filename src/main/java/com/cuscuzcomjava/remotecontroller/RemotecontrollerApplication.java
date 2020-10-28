3ecf3aeda21b03e6ca62123fcdb4d48f46c172b5
JoelMarSDS <joel.mar.s.d.s@gmail.com>
2020-10-28 17:34:41 -0300

    Primary commit project

---
+++ src/main/java/com/cuscuzcomjava/remotecontroller/RemotecontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.cuscuzcomjava.remotecontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemotecontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemotecontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuscuzcomjava/remotecontroller/RemotecontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuscuzcomjava.remotecontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemotecontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
