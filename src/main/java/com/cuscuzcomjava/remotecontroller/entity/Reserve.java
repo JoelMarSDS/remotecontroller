ce11cc32272bfb67903d7f5b0b651a12148a9b13
JoelMarSDS <joel.mar.s.d.s@gmail.com>
2020-10-29 19:50:04 -0300

    Correção de erro na developer

--- src/main/java/com/cuscuzcomjava/remotecontroller/entity/Reserve.java
+++ src/main/java/com/cuscuzcomjava/remotecontroller/entity/Reserve.java
@@ -1,7 +1,5 @@
 package com.cuscuzcomjava.remotecontroller.entity;
 
-import jdk.jfr.Enabled;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
