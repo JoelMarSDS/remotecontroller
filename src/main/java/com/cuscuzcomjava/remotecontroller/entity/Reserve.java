f0b90900fb759286840521c4879e5932371c06d3
DeMesquita <nayaramesquita2323@gmail.com>
2020-10-29 21:14:12 -0300

    add ActressController (create-actress and actress-reserves mapping)

--- src/main/java/com/cuscuzcomjava/remotecontroller/entity/Reserve.java
+++ src/main/java/com/cuscuzcomjava/remotecontroller/entity/Reserve.java
@@ -1,15 +1,12 @@
 package com.cuscuzcomjava.remotecontroller.entity;
 
-<<<<<<< HEAD
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-=======
 import com.sun.istack.NotNull;
 
 import javax.persistence.*;
->>>>>>> creatEntities
 import java.time.Instant;
 
 @Entity
